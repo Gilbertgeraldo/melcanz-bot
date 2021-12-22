c70a19147bac61d69df018c640a847c1c883c3ba
GILBERTSAJA <93893614+Gilbertgeraldo@users.noreply.github.com>
2021-12-22 12:53:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6283128734012', '6287755080455']
+global.owner = ['6281235136510', '6281235136510']
 
 global.APIs = {
   amel: 'https://melcanz.com',
@@ -13,12 +13,12 @@ global.APIKeys = {
 }
 
 global.packname = 'ig @ariffb.id @melcanz.io'
-global.author = '© ariffb & melcanz'
-global.wm = '© ariffb & melcanz'
+global.author = '© Gilbert'
+global.wm = '© Gilbert'
 global.eror = '*Error*'
-global.wait = '*tunggu..*'
+global.wait = '*tunggu bang hehe..*'
 global.benar = '✅'
-global.salah = '❌'
+global.salah = '❌, salah bro mangat yooooo'
 global.dikit = 'dikit lagi'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
 
